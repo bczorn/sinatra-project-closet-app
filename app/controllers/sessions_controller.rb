d8a22aa9039dc3d5c32cdfa8cb1f07d153c9009d
bczorn <bczorn@gmail.com>
2021-02-27 16:43:38 -0600

    created get /login and started post /login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,24 @@
 class SessionsController < ApplicationController
 
     get '/signup' do
-        erb :signup
+        erb :"/sessions/signup"
     end
 
+    get '/login' do
+        erb :"/sessions/login"
+    end
+
+    post '/login' do
+        # find user
+        @user = User.find_by(username: params[:user][:username])
+        # validate login info
+
+        if @user && @user.authenticate(params[:user][:password])
+            # true - successfully log in
+        else
+            #redirect
+        end
+
+    end
 
 end
